ef656855f6d4813f5dcf7a390b8488a20a10dd64
Lebohang Lesenya <lebohanglesenya0@gmail.com>
2021-06-18 19:37:52 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		{
 			if (alphabet != 'e' && alphabet != 'q')
-				putchar(letter);
+				putchar(alphabet);
 		}
 		putchar('\n');
 
