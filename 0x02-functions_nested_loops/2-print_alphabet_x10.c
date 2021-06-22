080dae0f0530346f25bb11394da84b38cb4980c5
Lebohang <lebohanglesenya21@gmail.com>
2021-06-22 10:27:28 -0700

    First commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print all small alphabets 10x
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i;
+	int j;
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 26; i++)
+		{
+			_putchar(alphabets[j]);
+		}
+	}
+	_putchar('\n');
+}
