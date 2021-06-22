d9c95bfae4905ffe18de08ff198595ed9bbc4fab
Lebohang <lebohanglesenya21@gmail.com>
2021-06-22 12:28:31 -0700

    First commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * Jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Return: 0 (program executed without errors)
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+					break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
