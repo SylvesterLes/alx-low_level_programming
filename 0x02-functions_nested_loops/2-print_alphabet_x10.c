67ccc396ee341357997b4b6570e1c4225d78bac6
Lebohang Lesenya <lebohanglesenya0@gmail.com>
2021-06-22 19:37:57 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,10 +12,10 @@ void print_alphabet_x10(void)
 
 	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 26; i++)
+		for (j = 0; j < 26; j++)
 		{
 			_putchar(alphabets[j]);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
