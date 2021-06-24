904dc82087036de37e9e936887fa5037fd5cec5f
Lebohang <lebohanglesenya21@gmail.com>
2021-06-24 07:34:47 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - checks if number provided is negative or positive
+ *
+ * Return: 0 (Executed successfully)
+ */
+int main(void)
+{
+	int num;
+	
+	num = 0;
+	positive_or_negative(num);
+	return (0);
+}
+
