f5ea5c2aa717661734e22109bb25ea103839449c
Lebohang <lebohanglesenya21@gmail.com>
2021-06-30 14:00:52 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: char value
+ * @src: char value
+ * @i: int value
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(src + i) != '\0')
+	{
+		i++;
+	}
+	for ( ; j < i ; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
