957b32b262454b53a145d24debd7919259e6f5d5
Lebohang <lebohanglesenya21@gmail.com>
2021-06-30 12:42:35 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - takes a pointer as parameter and 
+ * updates the value it points to to 98
+ * @n: our int parameter
+ * Return: (0)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
