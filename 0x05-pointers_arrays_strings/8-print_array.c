0fdaa4e7afd84418e7f59ac24e80b380be64960b
Lebohang <lebohanglesenya21@gmail.com>
2021-06-30 13:54:29 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+ * print_array - prints elements of an array
+ * @a: integer value
+ * @n: integer value
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
