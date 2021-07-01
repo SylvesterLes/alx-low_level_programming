05359b0e257cf91acf535ff135c70740521d6e72
Lebohang <lebohanglesenya21@gmail.com>
2021-07-01 10:33:33 -0700

    first commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: character value
+ * @src: character value
+ * @n: mteger value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
