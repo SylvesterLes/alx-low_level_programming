7ab0e48438cdbf108009b2571e19b39fa6c4e1db
Lebohang <lebohanglesenya21@gmail.com>
2021-07-12 11:52:20 -0700

    first commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * evaluate -validates correct input
+ * @user_input: commad line argument
+ *
+ * Return: 0 / 1
+ **/
+int evaluate(char *user_input)
+{
+	int i;
+	int num;
+	int length;
+
+	i = 0;
+	num = 0;
+	length = strlen(user_input);
+	while (i < length)
+	{
+		if (user_input[i] < '0' || user_input[i] > '9')
+		{
+			return (-1);
+		}
+		else
+		{
+			num = num * 10 + (user_input[i] - '0');
+		}
+		i++;
+	}
+	return (num);
+}
+/**
+ * main - adds positive numbers.
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ *
+ * Return: result /  1
+ **/
+int main(int argc, char *argv[])
+{
+	int i;
+	int num;
+	int sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		evaluate(argv[i]);
+		if (num == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
