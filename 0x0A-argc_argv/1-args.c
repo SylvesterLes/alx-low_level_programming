7df036e5b2ddbae02305fec3a51a11fa509559d2
Lebohang <lebohanglesenya21@gmail.com>
2021-07-12 10:59:36 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: arguemnet count
+ * @argv: array of pointer to arguement strings
+ *
+ * Return: 0 (Always)
+ **/
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
