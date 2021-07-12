f7c5678ad09c5e798f0945583217cc9f8b58bef6
Lebohang <lebohanglesenya21@gmail.com>
2021-07-12 11:24:32 -0700

    first comit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiply two numbers
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ *
+ * Return: result / 1
+ **/
+int main(int argc, char *argv[])
+{
+	int num_one;
+	int num_two;
+        int result;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return (0);
+}
