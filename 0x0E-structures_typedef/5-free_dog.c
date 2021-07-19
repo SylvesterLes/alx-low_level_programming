b5dbceec900673f57837006def21732c6190c044
Lebohang <lebohanglesenya21@gmail.com>
2021-07-19 09:50:44 -0700

    first commit

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees dogs.
+ * @d: pointer to dog of type dog_t.
+ *
+ * Return: 0 (ALways)
+ **/
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
